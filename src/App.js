65d304d80ae209f19862e1778a9d1072ca66024a
cristina-q <cristinamihaela@yandex.com>
2021-01-24 23:31:22 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,84 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const App = (props) => {
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 },
+    ],
+
+    otherState: 'some other value',
+    showPersons: false,
+  };
+
+  switchNameHandler = () => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximillian';
+    this.setState({
+      persons: [
+        { name: 'Laralam', age: 0 },
+        { name: 'Manu', age: 101 },
+        { name: 'Sue', age: 303 },
+      ],
+    });
+  };
+
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Yana', age: 35 },
+        { name: event.target.value, age: 35 },
+        { name: 'Katia', age: 309 },
+      ],
+    });
+  };
+
+  togglerPersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
+
+  render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+    };
+
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person) => {
+            return <Person name={person.name} age={person.age}></Person>;
+          })}
+        </div>
+      );
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi</h1>
+        <p>This is working</p>
+        <button style={style} onClick={this.togglerPersonsHandler}>
+          Toggle Persons
+        </button>
+        {persons}
+      </div>
+    );
+  }
+}
+
+export default App;
+
+// ------------------------------------------ Hooks
+/* const App = (props) => {
   const [personState, setpersonsState] = useState({
     persons: [
       { name: 'Lara', age: 0 },
@@ -41,31 +117,7 @@ const App = (props) => {
   );
   // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hello mai'));
 };
-
-export default App;
-
-/* state = {
-  persons: [
-    { name: 'Lara', age: 0 },
-    { name: 'Manu', age: 101 },
-    { name: 'Sue', age: 3 },
-  ],
-
-  otherState: 'some other value',
-};
-
-switchNameHandler = () => {
-  // console.log('Was clicked!');
-  // DON'T DO THIS: this.state.persons[0].name = 'Maximillian';
-  this.setState({
-    persons: [
-      { name: 'Laralam', age: 0 },
-      { name: 'Manu', age: 101 },
-      { name: 'Sue', age: 303 },
-    ],
-  });
-}; */
-
+ */
 // import './App.css';
 
 // function App() {
